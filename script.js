7fbc5b680328ec43aa6ec8c38afe98c21a062acb
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-02 11:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 function createTable() {
-    //Write your code here
-	// Prompt for the number of rows
   const numRows = window.prompt("Input number of rows", 1);
-
-  // Prompt for the number of columns
-  const numCols = window.prompt("Input number of columns",1);
-
-  // Clear the existing table
- // table.innerHTML = '';
-
-  // Create the table rows and columns
-  for (let i = 0; i < parseInt(numRows,10); i++) {
-	  // Get the table element
+  const numCols = window.prompt("Input number of columns", 1);
   
-const table = document.getElementById('myTable').insertRow(i);
-    for (let j = 0; j < parseInt(numCols,10); j++) {
-      const cell = table.insertCell(j);
+  const table = document.getElementById('myTable');
+  table.innerHTML = ''; // Clear the table before creating a new one
+
+  for (let i = 0; i < parseInt(numRows, 10); i++) {
+    const row = table.insertRow(i);
+    for (let j = 0; j < parseInt(numCols, 10); j++) {
+      const cell = row.insertCell(j);
       cell.innerHTML = `Row-${i} Column-${j}`;
     }
   }
@@ -26,4 +19,12 @@ const table = document.getElementById('myTable').insertRow(i);
 
 // // Add click event listener to the button
 // createBtn.addEventListener('click', createTable);
-}
+
+
+// function createTable() { const numRows = window.prompt("Input number of rows", 1); const numCols = window.prompt("Input number of columns",1); for (let i = 0; i < parseInt(numRows,10); i++) { const table = document.getElementById('myTable').insertRow(i);
+// for (let j = 0; j < parseInt(numCols,10); j++) {
+// const cell = table.insertCell(j);
+// cell.innerHTML = `Row-${i} Column-${j}`;
+// }
+// }
+// }
